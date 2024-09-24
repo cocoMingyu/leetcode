ba7d4078d236c89b77177977de02d05f1df55e0d
kn <1123542383@qq.com>
2024-09-24 17:36:06 +0800

    对称树

---
+++ src/main/java/tree/DiameterOfBinaryTree.java
@@ -0,0 +1,42 @@
+package tree;
+
+/**
+ * @ Author : kn
+ * @ Description :二叉树的直径
+ * 简单
+ * 给你一棵二叉树的根节点，返回该树的 直径 。
+ * 二叉树的 直径 是指树中任意两个节点之间最长路径的 长度 。这条路径可能经过也可能不经过根节点 root 。
+ * 两节点之间路径的 长度 由它们之间边数表示。
+ *
+ * 示例 1：
+ * 输入：root = [1,2,3,4,5]
+ * 输出：3
+ * 解释：3 ，取路径 [4,2,1,3] 或 [5,2,1,3] 的长度。
+ *
+ * 示例 2：
+ * 输入：root = [1,2]
+ * 输出：1
+ *
+ * 提示：
+ * 树中节点数目在范围 [1, 104] 内
+ * -100 <= Node.val <= 100
+ * @ Date : 2024/9/23 15:17
+ */
+public class DiameterOfBinaryTree {
+
+    /**
+     * 找到两条最长的树，以及两个树的根节点
+     * @param root
+     * @return
+     */
+    public int diameterOfBinaryTree(TreeNode root) {
+        int leftLength=0;
+        int rightLength=0;
+
+        return 0;
+    }
+
+    public void sumLength(TreeNode left,TreeNode right,int leftLength,int rightLength){
+        if
+    }
+}
--- src/main/java/tree/IsSymmetric.java
+++ src/main/java/tree/IsSymmetric.java
@@ -1,56 +1,68 @@
 package tree;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.util.Deque;
+import java.util.LinkedList;
 
 /**
  * @ Author : kn
- * @ Description :
+ * @ Description : 判断是否对称树
  * @ Date : 2024/9/21 15:22
  */
 public class IsSymmetric {
 
     public boolean isSymmetric(TreeNode root) {
-        List<Integer> list=new ArrayList<>();
-        addElement(root, list);
-        //由于根节点的存在数组个数是奇数，整除2是偶数，说明不是镜像。
-        if (list.size()%2==0){
-            return false;
+        //return check(root.left, root.right);
+        return checkByIteration(root);
+    }
+
+    public boolean check(TreeNode left, TreeNode right) {
+        if (left != null && right != null) {
+            //两个节点值相等，才继续递归
+            return right.val == left.val && check(left.left, right.right) && check(left.right, right.left);
+        } else {
+            return left == null && right == null;
         }
-        for (int i = 0; i < list.size()/2; i++) {
-            if (!Objects.equals(list.get(i),list.get(list.size()-i-1))){
+    }
+
+
+    public boolean checkByIteration(TreeNode root){
+        //使用迭代比较是否对称,根节点添加两次，比较两个节点的对称节点的值
+        Deque<TreeNode> deque=new LinkedList<>();
+        deque.offer(root);
+        deque.offer(root);
+        while (!deque.isEmpty()){
+            TreeNode left = deque.poll();
+            TreeNode right = deque.poll();
+
+            if (left==null&&right==null){
+                continue;
+            }
+
+            if (left==null||right==null||right.val!= left.val){
                 return false;
             }
+
+            //队列添加对称节点，两两一组
+            deque.offer(left.left);
+            deque.offer(right.right);
+
+            deque.offer(left.right);
+            deque.offer(right.left);
         }
         return true;
     }
 
-    public void addElement(TreeNode root, List<Integer> list) {
-        if (root == null) {
-            list.add(null);
-            return;
-        }
-        //左右节点都是空，是叶子结点，跳过
-        if (root.left == null && root.right == null){
-            list.add(root.val);
-        }else {
-            addElement(root.left, list);
-            list.add(root.val);
-            addElement(root.right, list);
-        }
-    }
 
     public static void main(String[] args) {
         TreeNode root = new TreeNode(5);
         root.left = new TreeNode(4);
-        root.right = new TreeNode(1);
-        //root.left.left = new TreeNode(3);
+        root.right = new TreeNode(4);
+        root.left.left = new TreeNode(3);
         root.left.right = new TreeNode(1);
-        root.left.right.left = new TreeNode(2);
-        //root.right.left = new TreeNode(3);
-        root.right.right = new TreeNode(4);
-        root.right.right.left = new TreeNode(2);
+        root.right.left = new TreeNode(1);
+        root.right.right = new TreeNode(3);
+        root.right.right.left = new TreeNode(3);
+
         System.out.println(new IsSymmetric().isSymmetric(root));
-   }
+    }
 }
